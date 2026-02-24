9c7dddcfd3f477b2623bb29b5702a40f13aacdf6
Варвара <barbara_littel@mail.ru>
2026-02-24 16:39:57 +0300

    Add Bitcoin data loader

--- src/loader.py
+++ src/loader.py
@@ -1,1 +1,15 @@
-print("Loading data from http://source.com")
+import pandas as pd
+import os
+
+def load_bitcoin_data():
+    path = 'src/bitcoin_information.csv'
+    if os.path.exists(path):
+        # В учебных целях просто имитируем чтение
+        print(f"Загрузка данных из {path}...")
+        # df = pd.read_csv(path) 
+        print("Данные успешно загружены. Начинаем анализ волатильности.")
+    else:
+        print("Файл с данными не найден!")
+
+if __name__ == "__main__":
+    load_bitcoin_data()
