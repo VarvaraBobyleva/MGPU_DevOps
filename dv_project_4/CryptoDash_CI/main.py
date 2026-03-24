4a2a967049434fd5f8001551230a7e3e1cba3fc0
VarvaraBobyleva <42711373+VarvaraBobyleva@users.noreply.github.com>
2026-03-24 18:09:57 +0300

    Update main.py

--- dv_project_4/CryptoDash_CI/main.py
+++ dv_project_4/CryptoDash_CI/main.py
@@ -16,7 +16,7 @@ def get_data():
     conn = psycopg2.connect(
         dbname=os.getenv('DB_NAME'),
         user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
+        password=os.getenv('DB_PASS'),
         host=os.getenv('DB_HOST')
     )
 
